3c371fc7d2296f9c4f8bdf988e283c3d5a8b5a82
Ahyoung-Kim <eileel@naver.com>
2022-08-05 22:40:24 +0900

    2022-08-05

--- bitmask/동아리실.cpp
+++ bitmask/동아리실.cpp
@@ -1,113 +1,79 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <string>
 #include <cstring>
 #include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
 #define MOD 1000000007
 
-int n;
-long long cnt;
-char str[10001];
+int dp[10001][17];
+string str;
 
-void printSubsets(char arr[], int n)
+int solution()
 {
-  // arr[4] = { 'A', 'B', 'C', 'D' }, n = 4
+  int ret = 0;
 
-  // 1 << n : 원소 n 개의 조합 개수
-  // n이 4일 때 i: 0~15 : 0000 ~ 1111
-  for (int i = 0; i < (1 << n); ++i)
+  cin >> str;
+
+  for (int day = 0; day < str.size(); day++)
   {
-    printf("{ ");
-    // & 연산자로 비트마킹
-    for (int j = 0; j < n; ++j)
+    // DCBA
+    int admin = 1 << (str[day] - 'A');
+
+    for (int i = 1; i < 16; i++)
     {
-      // 1 << j: 인덱스 j번째 비트마킹 0번째면 0001
-      if (i & (1 << j))
-        printf("%c ", arr[j]);
-    }
-    printf("}\n");
-  }
-}
 
-void printBin(int num)
-{
-  printf("{ ");
-  for (int i = 3; i >= 0; i--)
-  {
-    if (1 & (num >> i))
-    {
-      printf("1 ");
-    }
-    else
-    {
-      printf("0 ");
-    }
-  }
-  printf(" }, ");
-}
+      // 1일차: 'A"랑 첫째날 관리자가 모두 포함된 경우
+      if (day == 0)
+      {
+        if ((i & 1) && (admin & i))
+        {
+          dp[day][i] = 1;
+        }
+        continue;
+      }
 
-void dfs(int depth, int num)
-{
-  if (depth == n)
-  {
-    cnt++;
-    return;
-  }
+      // 전날 i 사람들이 참여했을 때 불가능한 경우일때
+      if (dp[day - 1][i] == 0)
+        continue;
 
-  int next = 8 >> (str[depth] - 65);
-
-  for (int i = next; i < 16; i++)
-  {
-    if ((num & i) && (next & i))
-    {
-      dfs(depth + 1, i);
-    }
-  }
-}
-
-void solution()
-{
-
-  // 첫째날은 A가 키를 가지고 있음
-  // start = 1000
-  int start = 8;
-
-  // A = 0  -> 1000  -> 8 >> 0
-  // B = 1  -> 0100  -> 8 >> 1
-  // C = 2  -> 0010  -> 8 >> 2
-  // D = 3  -> 0001  -> 8 >> 3
-  int first = 8 >> (str[0] - 65);
-
-  int num = start | first;
-
-  for (int i = num; i < 16; i++)
-  {
-    if ((start & i) && (first & i))
-    {
-      dfs(1, i);
+      // 2일차 ~
+      for (int j = 1; j < 16; j++)
+      {
+        if ((i & j) && (admin & j))
+        {
+          dp[day][j] += dp[day - 1][i];
+          dp[day][j] %= MOD;
+        }
+      }
     }
   }
 
-  cnt %= MOD;
+  for (int i = 1; i < 16; i++)
+  {
+    ret += dp[str.size() - 1][i];
+    ret %= MOD;
+  }
+
+  memset(dp, 0, sizeof(dp));
+  return ret;
 }
 
 int main(void)
 {
-  int T;
+  int T, N;
 
   cin >> T;
 
-  for (int tc = 1; tc <= T; tc++)
+  for (int t = 1; t <= T; t++)
   {
-    cin >> str;
-    n = strlen(str);
-    cnt = 0;
 
-    solution();
-
-    printf("#%d %d\n", tc, cnt);
+    int ans = solution();
+    printf("#%d %d\n", t, ans);
   }
 
   return 0;
--- graph/프로세서.cpp
+++ graph/프로세서.cpp
@@ -9,332 +9,163 @@
 using namespace std;
 
 int N;
-int processor[13][13];
-bool visit[13][13];
+int map[13][13];
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
+vector<pair<int, int>> cores;
 
-// first: 해당 방향으로 전선을 연결했을 때의 길이
-// second: 방향 남 0, 북 1, 동 2, 서 3
-typedef pair<int, int> dir;
+int maxC;
+int minL;
 
-struct core
+int check(int x, int y, int tx, int ty, int val, int right)
 {
-  int x;
-  int y;
-  vector<dir> direction;
+  int i, j;
+  int ret = 0;
 
-  void set(int px, int py)
-  {
-    x = px;
-    y = py;
-
-    for (int i = 0; i < 4; i++)
-    {
-      int tx = x + dx[i];
-      int ty = y + dy[i];
-
-      if (tx < 0 || tx >= N || ty < 0 || ty >= N)
+  if (x == tx)
+  { // y축 방향으로 이동
+    if (y < ty)
+    { //남
+      for (i = y + 1; i < N; i++)
       {
-        direction.push_back({0, i});
-        continue;
-      }
-
-      int len = 0;
-      if (i == 0)
-      { // south
-        for (int j = ty; j < N; j++)
+        if (map[i][x] != right)
         {
-          len++;
-          if (processor[j][tx] == 1)
+          for (j = i - 1; j > y; j--)
           {
-            len = -1;
-            break;
+            map[j][x] = right;
           }
+          return 0;
         }
+        map[i][x] = val;
+        ret++;
       }
-      else if (i == 1)
-      { // north
-        for (int j = ty; j >= 0; j--)
-        {
-          len++;
-          if (processor[j][tx] == 1)
-          {
-            len = -1;
-            break;
-          }
-        }
-      }
-      else if (i == 2)
-      { // east
-        for (int j = tx; j < N; j++)
-        {
-          len++;
-          if (processor[ty][j] == 1)
-          {
-            len = -1;
-            break;
-          }
-        }
-      }
-      else
-      { // west
-        for (int j = tx; j >= 0; j--)
-        {
-          len++;
-          if (processor[ty][j] == 1)
-          {
-            len = -1;
-            break;
-          }
-        }
-      }
-
-      direction.push_back({len, i});
     }
-
-    sort(direction.begin(), direction.end());
-  }
-
-  void print_core()
-  {
-
-    printf("좌표: (%d, %d)\n", x, y);
-    printf("방향 - 0 남, 1 북, 2 동, 3 서\n");
-
-    for (int i = 0; i < 4; i++)
-    {
-      printf("{%d, %d}, ", direction[i].first, direction[i].second);
+    else
+    { // 북
+      for (i = y - 1; i >= 0; i--)
+      {
+        if (map[i][x] != right)
+        {
+          for (j = i + 1; j < y; j++)
+          {
+            map[j][x] = right;
+          }
+          return 0;
+        }
+        map[i][x] = val;
+        ret++;
+      }
     }
-    printf("\n");
   }
-};
-
-vector<core> cores;
-
-void print_cores()
-{
-  for (int i = 0; i < cores.size(); i++)
-  {
-    core temp = cores[i];
-    temp.print_core();
+  else if (y == ty)
+  { // x축 방향으로 이동
+    if (x < tx)
+    { // 동
+      for (i = x + 1; i < N; i++)
+      {
+        if (map[y][i] != right)
+        {
+          for (j = i - 1; j > x; j--)
+          {
+            map[y][j] = right;
+          }
+          return 0;
+        }
+        map[y][i] = val;
+        ret++;
+      }
+    }
+    else
+    { // 서
+      for (i = x - 1; i >= 0; i--)
+      {
+        if (map[y][i] != right)
+        {
+          for (j = i + 1; j < x; j++)
+          {
+            map[y][j] = right;
+          }
+          return 0;
+        }
+        map[y][i] = val;
+        ret++;
+      }
+    }
   }
+
+  return ret;
 }
 
-void print_map()
+void dfs(int idx, int cnum, int lnum)
 {
-  for (int i = 0; i < N; i++)
+
+  if (idx == cores.size())
   {
-    for (int j = 0; j < N; j++)
+
+    if (maxC < cnum)
     {
-      printf("%d ", processor[i][j]);
+      maxC = cnum;
+      minL = lnum;
     }
-    printf("\n");
+    else if (maxC == cnum)
+    {
+      if (minL > lnum)
+      {
+        minL = lnum;
+      }
+    }
+    return;
   }
-  printf("---------------------------------\n");
+
+  bool flag = false;
+  pair<int, int> curr = cores[idx];
+
+  for (int i = 0; i < 4; i++)
+  {
+    int tx = curr.first + dx[i];
+    int ty = curr.second + dy[i];
+
+    if (map[ty][tx])
+      continue;
+
+    int len = check(curr.first, curr.second, tx, ty, 2, 0);
+    if (len != 0)
+    {
+      flag = true;
+      dfs(idx + 1, cnum + 1, lnum + len);
+      check(curr.first, curr.second, tx, ty, 0, 2);
+    }
+  }
+
+  if (!flag)
+  { // 네 방향 중 전선을 어디에도 잇지 못할 때
+    dfs(idx + 1, cnum, lnum);
+  }
 }
 
 void input()
 {
-
-  vector<pair<int, int>> coord;
+  cin >> N;
 
   cores.clear();
-  memset(visit, false, sizeof(visit));
+
+  maxC = 0;
+  minL = 1000000000;
 
   for (int i = 0; i < N; i++)
   {
     for (int j = 0; j < N; j++)
     {
-      cin >> processor[i][j];
+      cin >> map[i][j];
 
-      if (processor[i][j] == 1)
+      if (i == 0 || j == 0 || i == N - 1 || j == N - 1)
+        continue;
+
+      if (map[i][j] == 1)
       {
-        coord.push_back({j, i});
+        cores.push_back({j, i});
       }
     }
-  }
-
-  for (int i = 0; i < coord.size(); i++)
-  {
-    core ncore;
-    ncore.set(coord[i].first, coord[i].second);
-    cores.push_back(ncore);
-  }
-}
-
-int set_line(int x, int y, int _dir, int val, int size)
-{
-  int i;
-  int cnt = 0;
-
-  switch (_dir)
-  {
-  case 0: // south
-    for (i = y + 1; i < N && cnt < size; i++)
-    {
-      if (processor[i][x] == 1)
-        return cnt;
-
-      if (val == -1 && processor[i][x] == -1)
-        return cnt;
-
-      processor[i][x] = val;
-      cnt++;
-    }
-    break;
-  case 1: // north
-    for (i = y - 1; i >= 0 && cnt < size; i--)
-    {
-      if (processor[i][x] == 1)
-        return cnt;
-
-      if (val == -1 && processor[i][x] == -1)
-        return cnt;
-      processor[i][x] = val;
-      cnt++;
-    }
-    break;
-  case 2: // east
-    for (i = x + 1; i < N && cnt < size; i++)
-    {
-      if (processor[y][i] == 1)
-        return cnt;
-
-      if (val == -1 && processor[y][i] == -1)
-        return cnt;
-      processor[y][i] = val;
-      cnt++;
-    }
-    break;
-  case 3: // west
-    for (i = x - 1; i >= 0 && cnt < size; i--)
-    {
-      if (processor[y][i] == 1)
-        return cnt;
-
-      if (val == -1 && processor[y][i] == -1)
-        return cnt;
-      processor[y][i] = val;
-      cnt++;
-    }
-    break;
-  default:
-    break;
-  }
-
-  return cnt;
-}
-
-int get_size(int x, int y, int _dir)
-{
-  switch (_dir)
-  {
-  case 0:
-    return N - y - 1;
-    break;
-  case 1:
-    return y;
-    break;
-  case 2:
-    return N - x - 1;
-    break;
-  case 3:
-    return x;
-    break;
-  default:
-    break;
-  }
-}
-
-vector<pair<int, core>> path;
-bool flag;
-int ans;
-void dfs_search(core curr, int depth, int cnt, int _dir)
-{
-
-  if (flag)
-    return;
-
-  if (depth == cores.size() - 1)
-  {
-    flag = true;
-    ans = cnt;
-
-    /*printf("cnt: %d\n", cnt);
-    for (int i = 0; i < path.size(); i++) {
-      printf("direc: %d\n", path[i].first);
-      path[i].second.print_core();
-    }
-    printf("----------------------------------------\n");*/
-    printf("ans: %d\n", ans);
-    print_map();
-
-    return;
-  }
-
-  core next = cores[depth + 1];
-
-  for (int i = 0; i < 4; i++)
-  {
-    if (flag)
-      break;
-
-    int len = next.direction[i].first;
-    int direc = next.direction[i].second;
-
-    if (len == -1)
-    {
-      continue;
-    }
-
-    int size = get_size(next.x, next.y, direc);
-    int ret = set_line(next.x, next.y, direc, -1, size);
-
-    if (ret != size)
-    {
-      set_line(next.x, next.y, direc, 0, ret);
-      continue;
-    }
-
-    path.push_back({direc, next});
-    dfs_search(next, depth + 1, cnt + len, direc);
-    path.pop_back();
-    set_line(next.x, next.y, direc, 0, ret);
-  }
-}
-
-void dfs()
-{
-
-  flag = false;
-  ans = 0;
-
-  for (int i = 0; i < 4; i++)
-  {
-
-    if (flag)
-      break;
-
-    int len = cores[0].direction[i].first;
-    int _dir = cores[0].direction[i].second;
-
-    if (len == -1)
-      continue;
-
-    int size = get_size(cores[0].x, cores[0].y, _dir);
-    int ret = set_line(cores[0].x, cores[0].y, _dir, -1, size);
-
-    if (ret != size)
-    {
-      set_line(cores[0].x, cores[0].y, _dir, 0, ret);
-      continue;
-    }
-
-    path.push_back({_dir, cores[0]});
-    dfs_search(cores[0], 0, len, _dir);
-    path.pop_back();
-    set_line(cores[0].x, cores[0].y, _dir, 0, ret);
   }
 }
 
@@ -348,15 +179,11 @@ int main(void)
   for (int tc = 1; tc <= T; tc++)
   {
 
-    cin >> N;
-
     input();
 
-    // print_cores();
+    dfs(0, 0, 0);
 
-    dfs();
-
-    printf("#%d %d\n", tc, ans);
+    printf("#%d %d\n", tc, minL);
   }
 
   return 0;
